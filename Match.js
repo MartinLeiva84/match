5b91f00439c5ba40ab350228c360f46445067dd6
MartinLeiva84 <martin.e-leiva@hotmail.com>
2022-01-10 10:30:07 -0300

    Add files via upload

---
+++ Match.js
@@ -0,0 +1,17 @@
+const playbutton = document.getElementById('play');
+const pausebutton = document.getElementById('pause');
+const resetbutton = document.getElementById('reset');
+
+
+
+
+
+ const Try = 5;
+ const Convertion = 2;
+ const Penalty = 3;
+ const Trypenal = 5;
+
+
+ 
+     
+ 
